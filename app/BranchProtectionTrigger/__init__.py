fdb63c5b336a7fa9ce14fbcc1612fd37706802a5
nobuhikosekiya2 <nobuhiko.sekiya@appdynamics.com>
2021-03-08 21:49:18 +0900

    working code

--- app/BranchProtectionTrigger/__init__.py
+++ app/BranchProtectionTrigger/__init__.py
@@ -1,24 +1,57 @@
 import logging
-
+import requests
+import json
 import azure.functions as func
-
+import os
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    req_body = req.get_json()
+    action = req_body.get('action')
+    token = os.environ["token"]
+    user = os.environ["user"]
+    
+    try:
+        if not action == 'created':
+            return func.HttpResponse(f"Invalid request", status_code=422)
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        json_repository = req_body.get('repository')
+        full_name = json_repository.get('full_name')
+
+        # Protect main branch 
+        branch = 'main'
+        headers = {'Accept': 'application/vnd.github.v3+json'}
+        item_data = '{"required_status_checks":{"strict":true,"contexts":["contexts"]},"enforce_admins":true,"required_pull_request_reviews":null,"restrictions":null}'
+
+        url = 'https://api.github.com/repos/' + full_name + '/branches/' + branch + '/protection'
+
+        logging.info('Sending request: ' + url)
+        response = requests.put(url, headers=headers, data=item_data, auth=(user, token))
+        
+        if not response.status_code == 200:
+            logging.error("Failed. Status Code was " + str(response.status_code))
+            return func.HttpResponse(f"Failed protecting main branch", status_code=500)
+
+
+        # Create an Issue
+        url = 'https://api.github.com/repos/' + full_name + '/issues'
+
+        title = "Automatic main branch protection was applied"
+        body = "@nobuhikosekiya"
+        item_data = {'title':title, 'body':body}
+        json_data = json.dumps(item_data).encode("utf-8")
+
+        headers = {'Accept': 'application/vnd.github.v3+json'}
+        logging.info('Sending request: ' + url)
+        response = requests.post(url, headers=headers, data=json_data, auth=(user, token))
+                
+        if not response.status_code == 201:
+            logging.error('Failed. Status Code was ' + str(response.status_code))
+            return func.HttpResponse(f"Invalid request", status_code=422)
+
+        return func.HttpResponse(f"This HTTP triggered function executed successfully.")
+    except Exception as err:
+        logging.info("error: {0}".format(err))
+        return func.HttpResponse(f"Failed. ", status_code=500)
+    
